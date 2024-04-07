ca5d8f822d9232301a36e4680ac42dd80bc11bc0
pankaj <pkp20022@gmail.com>
2024-04-07 11:23:41 +0530

    added screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,8 @@ module.exports = {
     extend: {
       screens: {
         'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
         'laptop': '1075px',
-        // => @media (min-width: 1024px) { ... }
-  
         'desktop': '837px',
-        // => @media (min-width: 1280px) { ... }
       },
     },
   },
